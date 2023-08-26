df0ec1448f082b05d362e92737b18b239296a9d2
bahar <baharoruji2@gmail.com>
2023-08-27 02:03:05 +0330

    fix ajax bug

---
+++ Tasnim_Loan.Application/Services/Customers/Commands/EditUser/EditUserService.cs
@@ -0,0 +1,46 @@
+﻿using Tasnim_Loan.Application.Interfaces.Contexts;
+using Tasnim_Loan.Common.Dto;
+
+namespace Tasnim_Loan.Application.Services.Customers.Commands.EditUser
+{
+    public class EditUserService : IEditUserService
+    {
+        private readonly IDataBaseContext _context;
+
+        public EditUserService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+        public ResultDto Execute(RequestEdituserDto request)
+        {
+            var user = _context.Userss.Find(request.ID);
+            if (user == null)
+            {
+                return new ResultDto
+                {
+                    IsSuccess = false,
+                    Message = "کاربر یافت نشد"
+                };
+            }
+
+            //فیلد های نیاز به ویرایش 
+            user.FullName = request.FullName;
+       //     user.Password = request.Password;
+            user.National_Number = request.National_Number;
+            user.Unique_Payment_Identifier = request.Unique_Payment_Identifier;
+            user.Description = request.Description;
+            user.InsertTime = request.InsertionTime;
+           
+            _context.SaveChanges();
+
+            return new ResultDto()
+            {
+                IsSuccess = true,
+                Message = "ویرایش کاربر انجام شد"
+            };
+
+        }
+    }
+
+
+}
---
+++ Tasnim_Loan.Application/Services/Customers/Commands/EditUser/RequestEdituserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tasnim_Loan.Application.Services.Customers.Commands.EditUser
+{
+    public class RequestEdituserDto
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+      //  public string Password { get; set; }
+        public string National_Number { get; set; }
+        public int Unique_Payment_Identifier { get; set; }
+        public string Description { get; set; }
+        public DateTime InsertionTime { get; set; }
+    }
+
+
+}
