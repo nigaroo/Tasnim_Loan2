33f6b175c62b1e3c193c70cfc6eb75b379486135
negar safarzadeh <negarsafarzadeh7@gmail.com>
2023-09-04 00:13:51 +0330

    panelbahar

---
+++ EndPoint.Site/Areas/Admin/Controllers/PanelinfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.Site.Areas.Admin.Controllers
+{
+    public class PanelinfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
